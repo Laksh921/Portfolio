d8800859fdab4f4558b0b0686219259a688dbdd0
Lakshya Bansal <lakshyab9712@gmail.com>
2025-04-06 01:47:02 +0530

    Updated build

--- src/components/intro.js
+++ src/components/intro.js
@@ -10,7 +10,12 @@ const Intro = () => {
                 <span className="introText">I'm <span className="introName">Lakshya,</span> <br/> Website Designer</span>
                 <p className="introPara"> With a strong foundation in engineering principles and desire for continuous learning, <br />I am well-positioned to make meaningful contributions to the field and <br />create a positive impact on society.</p>
                 <div class="button-container">
-                <a><button className="btn"><img src={binning} alt="Hire Me" className="binning"/>Hire Me</button></a>
+                <a href="#contact">
+                    <button className="btn">
+                        <img src={binning} alt="Hire Me" className="binning" />
+                        Hire Me
+                    </button>
+                </a>
                 <a href="https://drive.google.com/file/d/17jI0fxfWdwvs0NA6jSi8VBuSO_nmm2pB/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                 <button className="Resume">My Resume</button></a>
                 </div>
